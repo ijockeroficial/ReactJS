2be9bf80b1fb4091c221fd48db4e157d76ed4ea8
Rhógeris Lisboa <ijockeroficial@gmail.com>
2021-02-12 10:50:26 -0300

    Tutorial ReactJS primeiros passos Canal DevSuperior

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
+import React, { useState } from 'react'
+import GitHubImage from './git.png'
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(`https://api.github.com/users/${search}`)
+    .then(response => response.json())
+    .then(userResponse => setUserData(userResponse))
+  }
+  console.log(userData)
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">GitHub Profile</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="input-group">
+            <input
+            type="text"
+            className="form-control"
+            required
+            value={search}
+            onChange={handleChange}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        {!userData && (
+          <img src={GitHubImage}
+          className="responsive rounded-circle"
+          alt=""
+          height="200"/>
+        )}
+     {userData && (
+       <div>
+          <img src={userData.avatar_url}
+        className="responsive rounded-circle"
+        alt=""
+        height="200"/>
+        <h1 className="pt-5">
+          <a href="https://github.com/ijockeroficial">
+            {userData.name}
+          </a>
+        </h1>
+        <h3>{userData.location}</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href={userData.blog} target="_new" className="text-info">
+          {userData.blog}
+
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       </div>
+     )}
+      </div>
     </div>
   );
 }
